170d10e0593b8b04bb72b80c494f5a5ffa75030a
ClassyApe <ansark873.ak@gmail.com>
2024-08-10 18:24:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const uniqid = require("uniqid");
 const app = express();
 
 // UAT environment
-const MERCHANT_ID = "PGTESTPAYUAT86";
-const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox"; 
-const SALT_INDEX = 1;
-const SALT_KEY = "96434309-7796-489d-8924-ab56988a6076";
+const MERCHANT_ID = "M22XB34TDN5Q1";
+const PHONE_PE_HOST_URL = "	https://api.phonepe.com/apis/hermes"; 
+const SALT_INDEX = 2;
+const SALT_KEY = "8c830028-50de-41ae-b5f7-c5493ee53ced";
 const APP_BE_URL = "https://classy-payments-dev.netlify.app"; // our application
 
 // setting up middleware
