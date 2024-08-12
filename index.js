6cc2540daa1ebf3bff626442edc742433b11dada
ClassyApe <ansark873.ak@gmail.com>
2024-08-12 23:56:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // UAT environment variables
 const MERCHANT_ID = "M22XB34TDN5Q1";
-const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/hermes"; 
+const PHONE_PE_HOST_URL = "https://api.phonepe.com/apis/hermes"; 
 const SALT_INDEX = 1;
 const SALT_KEY = "8c830028-50de-41ae-b5f7-c5493ee53ced";
 const APP_BE_URL = "https://classypayments.com"; // our application
